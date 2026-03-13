ed3723920d6ebe7a097d5fcf1a363543c80a34f3
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-13 07:20:27 +0100

    feat: Upgrade to Horde 6 composer native and drop PEAR dependency.

---
+++ src/Beatnik.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2005-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @category Horde
+ * @package  Beatnik
+ * @author   Ben Klang <bklang@horde.org>
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+namespace Horde\Beatnik;
+
+/**
+ * Beatnik application API.
+ *
+ * This file provides a placeholder for the modern PSR-4 implementation
+ * of the Beatnik application.
+ *
+ * @category Horde
+ * @package  Beatnik
+ * @author   Ben Klang <bklang@horde.org>
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+class Beatnik
+{
+}
